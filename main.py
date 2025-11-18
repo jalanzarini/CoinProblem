8e824ff1b359b964c48c7ef08637f4c204a694bd
Alan <alanyoshiotara@gmail.com>
2025-11-18 19:15:02 -0300

    esqueci umas parada

---
+++ main.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+import os
+
+def main():
+    # Open images
+    img = cv2.imread('test_images/test1.jpg', cv2.IMREAD_COLOR_BGR)
+    fivecentscara = cv2.imread('coins/5centscara.jpg', cv2.IMREAD_GRAYSCALE)
+    fivecentscoroa = cv2.imread('coins/5centscoroa.jpg', cv2.IMREAD_GRAYSCALE)
+    fivecents = (fivecentscara, fivecentscoroa)
+
+    tencentscara = cv2.imread('coins/10centscara.jpg', cv2.IMREAD_GRAYSCALE)
+    tencentscoroa = cv2.imread('coins/10centscoroa.jpg', cv2.IMREAD_GRAYSCALE)
+    tencents = (tencentscara, tencentscoroa)
+
+    tfivecentscara = cv2.imread('coins/25centscara.jpg', cv2.IMREAD_GRAYSCALE)
+    tfivecentscoroa = cv2.imread('coins/25centscoroa.jpg', cv2.IMREAD_GRAYSCALE)
+    tfivecents = (tfivecentscara, tfivecentscoroa)
+
+    fftcentscara = cv2.imread('coins/50centscara.jpg', cv2.IMREAD_GRAYSCALE)
+    fftcentscoroa = cv2.imread('coins/50centscoroa.jpg', cv2.IMREAD_GRAYSCALE)
+    fftcents = (fftcentscara, fftcentscoroa)
+
+    onercara = cv2.imread('coins/1realcara.jpg', cv2.IMREAD_GRAYSCALE)
+    onercoroa = cv2.imread('coins/1realcoroa.jpg', cv2.IMREAD_GRAYSCALE)
+    oner = (onercara, onercoroa)
+
+    ref_coins = {
+        "5cents": fivecents,
+        "10cents": tencents,
+        "25cents": tfivecents,
+        "50cents": fftcents,
+        "1real": oner
+    }
+
+
+    cv2.imshow('output', img)
+
+    teste = cv2.HoughCircles(onercara, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
+    cv2.imshow('output2', teste)
+
+if __name__ == "__main__":
+    main()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
